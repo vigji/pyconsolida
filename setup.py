4490b5efb2169674898251cde0e9dd05682547f5
Luigi Petrucco <luigi.petrucco@gmail.com>
2025-02-27 15:15:38 +0100

    Bump version: 1.0.1 → 1.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md") as f:
 
 setup(
     name="pyconsolida",
-    version="1.0.1",
+    version="1.0.2",
     description="Aggregatore dati da budget cantiere I.CO.P",
     install_requires=[
         "pandas>=1.3.0",
